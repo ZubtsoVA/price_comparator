c7398976ed93acda2e8c00ba7330b066175cd7f5
KorepinaAnna <corepina.anya2018@gmail.com>
2026-05-27 23:18:04 +0300

    changed gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Фронтенд
+  root "pages#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Поиск
+  get  "/search",      to: "searches#show"
+  post "/cache/clear", to: "searches#clear_cache"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # История цен
+  get "/products/:id/price_history", to: "products#price_history"
 end
